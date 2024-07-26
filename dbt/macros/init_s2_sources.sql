d64a274417cfc50ed768393785454be86a51ba98
a.polyakov <avpgenium@yandex.ru>
2024-07-26 10:17:32 +0300

    add dbt example

--- dbt/macros/init_s2_sources.sql
+++ dbt/macros/init_s2_sources.sql
@@ -1,4 +1,4 @@
-{% macro init_s3_sources -%}
+{% macro init_s3_sources() -%}
 
     {% set sources = [
     'DROP table if exists src_covid_cases',
@@ -11,7 +11,17 @@
 	    cases UInt32,
 	    deaths UInt32,
 	    geo_id String
-    ) engine = S3(\'https://storage.yandexcloud.net/ch-data-course/covid_cases/raw_covid__cases.csv\', \'CSVWithNames\')'
+    ) engine = S3(\'https://storage.yandexcloud.net/ch-data-course/covid_cases/raw_covid__cases.csv\', \'CSVWithNames\')',
+    'DROP table if exists ref_country_codes',
+
+'create table ref_country_codes (
+	country String,
+	alpha_2code String,
+	alpha_3code String,
+	numeric_code UInt32,
+	latitude_avg Decimal(15, 4),
+	longitude_avg Decimal(15, 4)
+)  engine = S3(\'https://storage.yandexcloud.net/ch-data-course/covid_cases/reference/ref__country_codes.csv\', \'CSVWithNames\')'
     ]
     %}
 
